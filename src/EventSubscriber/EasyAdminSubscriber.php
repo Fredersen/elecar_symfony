382cb1ca04e62bf0551f1e1eff8a648f91f0c1b6
Fredersen <naim.jhuboo@gmail.com>
2023-02-14 10:47:26 +0100

    isheader solo

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Car;
+use App\Repository\CarRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class EasyAdminSubscriber implements  EventSubscriberInterface
+{
+    private $carRepository;
+
+    public function __construct(CarRepository $carRepository)
+    {
+        $this->carRepository = $carRepository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            AfterEntityPersistedEvent::class => ['setIsHeader'],
+            AfterEntityUpdatedEvent::class => ['setIsHeaderUpdated'],
+        ];
+    }
+
+    public function setIsHeader(AfterEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Car) || !$entity->isIsHeader()) {
+            return;
+        }
+
+        $cars = $this->carRepository->findAll();
+
+        foreach ($cars as $car) {
+            $car->setIsHeader(false);
+            $this->carRepository->save($car, true);
+        }
+
+        $entity->setIsHeader(true);
+        $this->carRepository->save($entity, true);
+    }
+
+    public function setIsHeaderUpdated(AfterEntityUpdatedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Car) || !$entity->isIsHeader()) {
+            return;
+        }
+
+        $cars = $this->carRepository->findAll();
+
+        foreach ($cars as $car) {
+            $car->setIsHeader(false);
+            $this->carRepository->save($car, true);
+        }
+
+        $entity->setIsHeader(true);
+        $this->carRepository->save($entity, true);
+    }
+}
